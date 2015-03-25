2db9ec03e7760cf331268dd122fe41def1bdb933
Volker Dobler <dr.volker.dobler@gmail.com>
2015-03-25 17:28:37 +0100

    Make debug output in failed test more readable.

--- suite_test.go
+++ suite_test.go
@@ -5,6 +5,7 @@
 package ht
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"testing"
@@ -29,8 +30,29 @@ func TestLoadSuite(t *testing.T) {
 
 	result := suite.ExecuteTests()
 	if result.Status != Pass {
-		s := pretty.Sprintf("% #v", result)
-		t.Fatalf("Unexpected problems result=\n%s\n", s)
+		for _, tr := range result.TestResults {
+			if tr.Status == Pass {
+				continue
+			}
+			fmt.Println("Test", tr.Name)
+			if tr.Error != nil {
+				fmt.Println("  Error: ", tr.Error)
+			} else {
+				for _, cr := range tr.CheckResults {
+					if cr.Status == Pass {
+						continue
+					}
+					fmt.Println("  Fail: ", cr.Name, cr.JSON, cr.Status, cr.Error)
+				}
+			}
+			tr.Response.Response.Request.TLS = nil
+			req := pretty.Sprintf("% #v", tr.Response.Response.Request)
+			fmt.Printf("  Request\n%s\n", req)
+			tr.Response.Response.Request = nil
+			tr.Response.Response.TLS = nil
+			resp := pretty.Sprintf("% #v", tr.Response.Response)
+			fmt.Printf("  Response\n%s\n", resp)
+		}
 	}
 
 	result.PrintReport(os.Stdout)
