a4af45325a14728b1fdc1ed9761df3e8478c9035
Volker Dobler <dr.volker.dobler@gmail.com>
2015-03-31 10:51:45 +0200

    Finalise spliting check test into relevant files.

--- check/html_test.go
+++ check/html_test.go
@@ -5,30 +5,10 @@
 package check
 
 import (
-	_ "image/jpeg"
-	_ "image/png"
 	"testing"
 
 	"github.com/vdobler/ht/response"
 )
-
-var responseTimeTests = []TC{
-	{response.Response{Duration: 10 * ms}, ResponseTime{Lower: 20 * ms}, nil},
-	{response.Response{Duration: 10 * ms}, ResponseTime{Lower: 2 * ms}, someError},
-	{response.Response{Duration: 10 * ms}, ResponseTime{Higher: 2 * ms}, nil},
-	{response.Response{Duration: 10 * ms}, ResponseTime{Higher: 20 * ms}, someError},
-	{response.Response{Duration: 10 * ms}, ResponseTime{Higher: 5 * ms, Lower: 20 * ms}, nil},
-	{response.Response{Duration: 10 * ms}, ResponseTime{Higher: 15 * ms, Lower: 20 * ms}, someError},
-	{response.Response{Duration: 10 * ms}, ResponseTime{Higher: 5 * ms, Lower: 8 * ms}, someError},
-	{response.Response{Duration: 10 * ms}, ResponseTime{Higher: 20 * ms, Lower: 5 * ms},
-		MalformedCheck{Err: someError}},
-}
-
-func TestResponseTime(t *testing.T) {
-	for i, tc := range responseTimeTests {
-		runTest(t, i, tc)
-	}
-}
 
 var hcr = response.Response{Body: []byte(`<!doctype html>
 <html>
