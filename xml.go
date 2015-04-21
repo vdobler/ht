dcd38ad2c584e5e0da190adefa03d4be245f0b3e
Volker Dobler <dr.volker.dobler@gmail.com>
2015-04-21 22:44:39 +0200

    Add basic support for testing XML documents
    The check is done by slecting an element/attribute via a XPath
    expression and applying our default Condition to the string
    representation of the first node found.

---
+++ xml.go
@@ -0,0 +1,56 @@
+// Copyright 2015 Volker Dobler.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// xml.go contains XPath checks against XML documents.
+
+package ht
+
+import "launchpad.net/xmlpath"
+
+func init() {
+	RegisterCheck(&XML{})
+}
+
+// ----------------------------------------------------------------------------
+// XML
+
+// XML allows to check XML request bodies.
+type XML struct {
+	// Path is a XPath expression understood by launchpad.net/xmlpath.
+	Path string
+
+	// Condition the first element addressed by Path must fullfill.
+	Condition
+
+	path *xmlpath.Path
+}
+
+func (x *XML) Execute(t *Test) error {
+	if t.Response.BodyErr != nil {
+		return CantCheck{t.Response.BodyErr}
+	}
+
+	root, err := xmlpath.Parse(t.Response.Body())
+	if err != nil {
+		return err
+	}
+
+	if s, ok := x.path.String(root); !ok {
+		return NotFound
+	} else if e := x.Fullfilled(s); err != nil {
+		return e
+	}
+
+	return nil
+}
+
+func (x *XML) Prepare() error {
+	p, err := xmlpath.Compile(x.Path)
+	if err != nil {
+		return err
+	}
+
+	x.path = p
+	return nil
+}
---
+++ xml_test.go
@@ -0,0 +1,48 @@
+// Copyright 2015 Volker Dobler.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package ht
+
+import "testing"
+
+var xmlr = Response{
+	BodyBytes: []byte(`<?xml version="1.0" encoding="UTF-8"?>
+<library>
+  <!-- Great book. -->
+  <book id="b0836217462" available="true">
+    <isbn>0836217462</isbn>
+    <title lang="en">Being a Dog Is a Full-Time Job</title>
+    <quote>I'd dog paddle the deepest ocean.</quote>
+    <author id="CMS">
+      <?echo "go rocks"?>
+      <name>Charles M Schulz</name>
+      <born>1922-11-26</born>
+      <dead>2000-02-12</dead>
+    </author>
+    <character id="PP">
+      <name>Peppermint Patty</name>
+      <born>1966-08-22</born>
+      <qualification>bold, brash and tomboyish</qualification>
+    </character>
+    <character id="Snoopy">
+      <name>Snoopy</name>
+      <born>1950-10-04</born>
+      <qualification>extroverted beagle</qualification>
+    </character>
+  </book>
+</library>
+`)}
+
+var xmlTests = []TC{
+	{xmlr, &XML{Path: "/library/book/isbn", Condition: Condition{Equals: "0836217462"}}, nil},
+	{xmlr, &XML{Path: "/library/book/character[2]/name", Condition: Condition{Equals: "Snoopy"}}, nil},
+	{xmlr, &XML{Path: "//book[author/@id='CMS']/title", Condition: Condition{Contains: "Dog"}}, nil},
+	{xmlr, &XML{Path: "/library/book/notthere", Condition: Condition{Contains: "a"}}, someError},
+}
+
+func TestXML(t *testing.T) {
+	for i, tc := range xmlTests {
+		runTest(t, i, tc)
+	}
+}
