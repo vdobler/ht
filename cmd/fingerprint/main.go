b1583f981f3bfa0e6d57acd77cc37164170b2e5b
Volker Dobler <dr.volker.dobler@gmail.com>
2015-07-30 15:30:34 +0200

    Add new command 'reconstruct'
    Reconstruct tries to invert fingerprint. The reverting via the -reconstruct option to the
    fingerprint command is removed as this was a bad idea.

--- cmd/fingerprint/main.go
+++ cmd/fingerprint/main.go
@@ -9,57 +9,22 @@ import (
 	"fmt"
 	"image"
 	_ "image/gif"
-	"image/jpeg"
+	_ "image/jpeg"
 	_ "image/png"
 	"os"
-	"strconv"
 
 	"github.com/vdobler/ht/fingerprint"
 )
 
-func usage() {
-	fmt.Fprintf(os.Stderr, `fingerprint: calculate image fingerprints
-
-Usage:
-    fingerprint <filename>...
-    fingerprint -reconstruct <hash> [ <width> <height> ]
-`)
-	os.Exit(1)
-}
-
 func main() {
 	if len(os.Args) == 1 {
-		usage()
-	}
-
-	if os.Args[1] == "-reconstruct" {
-		width, height, hash := 64, 64, ""
-		var err error
-		switch len(os.Args) {
-		case 3:
-			hash = os.Args[2]
-		case 5:
-			hash = os.Args[2]
-			width, err = strconv.Atoi(os.Args[3])
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s\n", err)
-				usage()
-			}
-			height, err = strconv.Atoi(os.Args[4])
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s\n", err)
-				usage()
-			}
-		default:
-			usage()
-		}
-		reconstruct(hash, width, height)
-		os.Exit(0)
+		fmt.Printf("Usage: %s filename....\n", os.Args[0])
+		os.Exit(1)
 	}
 
 	fmt.Println("#       Filename:  BMV-Hash         ColorHist-Hash")
 	for _, path := range os.Args[1:] {
-		fmt.Fprintf(os.Stderr, "%16s:  %s\n", path, compute(path))
+		fmt.Printf("%16s:  %s\n", path, compute(path))
 	}
 }
 
@@ -78,19 +43,4 @@ func compute(path string) string {
 	bmv := fingerprint.NewBMVHash(img)
 
 	return bmv.String() + " " + ch.String()
-}
-
-func reconstruct(hash string, width, height int) {
-	fmt.Fprintf(os.Stderr, "%d %d\n", width, height)
-	bmv, err := fingerprint.BMVHashFromString(hash)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Sorry, %q is not a BMV Hash value\n", hash)
-		os.Exit(1)
-	}
-	img := bmv.Image(width, height)
-	err = jpeg.Encode(os.Stdout, img, nil)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Problems writing image: %s\n", err.Error())
-		os.Exit(1)
-	}
 }
