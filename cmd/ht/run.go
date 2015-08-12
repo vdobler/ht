0781159ff9a6d796ba4bfc4b9567a4049e871bc8
Volker Dobler <dr.volker.dobler@gmail.com>
2015-08-12 08:30:02 +0200

    cmd/ht: use normal exec code during 'run' subcommand
    The 'run' subcommand creates an arteficial suite to run the individual test
    files, so there is no need to double the code for suite execution and result
    printing in run: Just hand over the autogenerated suite to the exec functions.
    
    While at it: Make 'run' understand the -D command line flag. Without the
    ability to set variables it is often impossible to run a single file test.

--- cmd/ht/run.go
+++ cmd/ht/run.go
@@ -4,15 +4,7 @@
 
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"sync"
-	"time"
-
-	"github.com/vdobler/ht/ht"
-)
+import "github.com/vdobler/ht/ht"
 
 var cmdRun = &Command{
 	Run:         runRun,
@@ -24,69 +16,13 @@ and executes the test (or the first of the unroled tests).
 	`,
 }
 
+func init() {
+	cmdRun.Flag.StringVar(&outputDir, "output", "",
+		"save results to `dirname` instead of timestamp")
+	addVariablesFlag(&cmdRun.Flag)
+	addVerbosityFlag(&cmdRun.Flag)
+}
+
 func runRun(cmd *Command, suites []*ht.Suite) {
-	if outputDir == "" {
-		outputDir = time.Now().Format("2006-01-02_15h04m05s")
-	}
-	os.MkdirAll(outputDir, 0766)
-	var wg sync.WaitGroup
-	for i := range suites {
-		if serialFlag {
-			suites[i].Execute()
-			if suites[i].Status > ht.Pass {
-				log.Printf("Suite %d %q failed: %s", i+1,
-					suites[i].Name,
-					suites[i].Error.Error())
-			}
-		} else {
-			wg.Add(1)
-			go func(i int) {
-				suites[i].Execute()
-				if suites[i].Status > ht.Pass {
-					log.Printf("Suite %d %q failed: %s", i+1,
-						suites[i].Name, suites[i].Error.Error())
-				}
-				wg.Done()
-			}(i)
-		}
-	}
-	wg.Wait()
-
-	total, totalPass, totalError, totalSkiped, totalFailed, totalBogus := 0, 0, 0, 0, 0, 0
-	for s := range suites {
-		t := fmt.Sprintf("Suite %d: %s", s+1, suites[s].Name)
-		fmt.Printf("\n%s\nFile %s\nStatus %s\n", ht.BoldBox(t, ""),
-			suites[s].Name, suites[s].Status)
-		for _, r := range suites[s].AllTests() {
-			r.PrintReport(os.Stdout)
-			switch r.Status {
-			case ht.Pass:
-				totalPass++
-			case ht.Error:
-				totalError++
-			case ht.Skipped:
-				totalSkiped++
-			case ht.Fail:
-				totalFailed++
-			case ht.Bogus:
-				totalBogus++
-			}
-			total++
-		}
-
-		dirname := outputDir + "/" + sanitizer.Replace(suites[s].Name)
-
-		fmt.Printf("Saveing result of suite %q to folder %q.\n", suites[s].Name, dirname)
-		err := os.MkdirAll(dirname, 0766)
-		if err != nil {
-			log.Panic(err)
-		}
-		err = suites[s].HTMLReport(dirname)
-		if err != nil {
-			log.Panic(err)
-		}
-	}
-	fmt.Printf("Total %d,  Passed %d, Skipped %d,  Errored %d,  Failed %d,  Bogus %d\n",
-		total, totalPass, totalSkiped, totalError, totalFailed, totalBogus)
-
+	runExecute(cmd, suites)
 }
