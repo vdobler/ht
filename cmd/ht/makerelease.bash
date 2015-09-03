c47158339e83f2467bd8f7deab9fb683d00aafe3
Volker Dobler <dr.volker.dobler@gmail.com>
2015-09-03 17:29:11 +0200

    all: use the oracle and doc tool to generate check documentation

--- cmd/ht/makerelease.bash
+++ cmd/ht/makerelease.bash
@@ -6,4 +6,6 @@ version=$(git describe)
 GO15VENDOREXPERIMENT="1" go build -v -ldflags "-X main.version=$version"
 # Pack binaries with github.com/pwaller/goupx
 goupx --strip-binary ht
+(cd ../../ht/; ./list-checks.bash;) > CheckDocumentation.txt
 ./ht version
+
--- ht/list-checks.bash
+++ ht/list-checks.bash
@@ -1,26 +1,19 @@
 #! /bin/bash
 
-checktypes=$(grep -E "^func \(.*\) Execute\(t \*Test\) error {" *.go \
-    | grep -v _test.go: \
-    | sed -e "s/\*//; s/)//" \
-    | cut -d " " -f 3 | sort)
+checks=$(oracle -pos ./check.go:#332 implements | \
+    cut -f 2 | cut -d " " -f 6 | tr -d "*" | sort)
 
-echo "Available Checks"
-echo "================"
-echo "$checktypes"
 
-for t in $checktypes; do
+for c in $checks; do
+    [[ -n "$c" ]] || continue
+    figlet -W -w 100 "$c"
+    go doc "$c"
+
     echo
     echo
-    sourcefile=$(grep -E -l "^type $t " *.go)
-    lines=$(awk  "
-BEGIN       { n=1; found=0; start=0; end=0; } 
-/^type $t struct {/ { found=1; n++; next; } 
-/^type $t / { found=1; end=n; n++; next; } 
-/^\$/       { n++; if(found==0){ start = n; } next; }
-/^}$/       { if(found==1 && end==0){ end = n; } n++; next;}
-            { n++; next; } 
-END         { printf \"%d,%dp\",start, end; }" $sourcefile)
-    sed -n $lines $sourcefile
-    echo "// (in file $sourcefile)"
-done
+done | grep -v -E "^func "
+
+echo "===================================================================================================="
+
+figlet -W -w 100 "Conditon"
+go doc Condition | grep -v -E "^func "
