955f97bde7e625d4986b32900ad63c8c037a0751
Volker Dobler <dr.volker.dobler@gmail.com>
2015-09-09 10:11:04 +0200

    cmd/ht: build for all platforms during release
    And beautify the output of makerelease.bash a bit.

--- cmd/ht/makerelease.bash
+++ cmd/ht/makerelease.bash
@@ -3,9 +3,32 @@
 set -e
 
 version=$(git describe)
-GO15VENDOREXPERIMENT="1" go build -v -ldflags "-X main.version=$version"
-# Pack binaries with github.com/pwaller/goupx
-goupx --strip-binary ht
+export GO15VENDOREXPERIMENT="1" 
+rm -f ht*
+
+echo "### Using $(go version)"
+
+echo
+echo "### Linux version"
+GOOS=linux GOARCH=amd64 go build -o ht_linux -ldflags "-X main.version=$version"
+# Pack binaries with goupx (github.com/pwaller/goupx) which
+# uses upx (http://upx.sourceforge.net)
+goupx --strip-binary ht_linux
+
+echo
+echo "### Mac OS X version"
+GOOS=darwin GOARCH=amd64 go build -o ht_darwin -ldflags "-X main.version=$version"
+
+echo
+echo "### Windows version"
+GOOS=windows GOARCH=amd64 go build -o ht_windows.exe -ldflags "-X main.version=$version"
+
+echo
+echo "### Check documentation"
 (cd ../../ht/; ./list-checks.bash;) > CheckDocumentation.txt
-./ht version
+ls -l CheckDocumentation.txt
+
+source <(go env)
+echo
+echo "Successfully built $(./ht_$GOHOSTOS version)"
 
