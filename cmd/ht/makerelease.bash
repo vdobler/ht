55c86910bad5bd5dd8f70dc227c3e81356fc4592
Volker Dobler <dr.volker.dobler@gmail.com>
2015-09-17 14:46:06 +0200

    cmd: drop debug information when building a release
    The windows binary size drops from 11 to 8 MB. Unfortunately darwnin is unaffected.
    Linux binaries are stripped anyway.

--- cmd/ht/makerelease.bash
+++ cmd/ht/makerelease.bash
@@ -8,20 +8,22 @@ rm -f ht*
 
 echo "### Using $(go version)"
 
+LDFLAGS="-X main.version=$version -s"
+
 echo
 echo "### Linux version"
-GOOS=linux GOARCH=amd64 go build -o ht_linux -ldflags "-X main.version=$version"
+GOOS=linux GOARCH=amd64 go build -o ht_linux -ldflags "$LDFLAGS"
 # Pack binaries with goupx (github.com/pwaller/goupx) which
 # uses upx (http://upx.sourceforge.net)
 goupx --strip-binary ht_linux
 
 echo
 echo "### Mac OS X version"
-GOOS=darwin GOARCH=amd64 go build -o ht_darwin -ldflags "-X main.version=$version"
+GOOS=darwin GOARCH=amd64 go build -o ht_darwin -ldflags "$LDFLAGS"
 
 echo
 echo "### Windows version"
-GOOS=windows GOARCH=amd64 go build -o ht_windows.exe -ldflags "-X main.version=$version"
+GOOS=windows GOARCH=amd64 go build -o ht_windows.exe -ldflags "$LDFLAGS"
 
 echo
 echo "### Check documentation"
