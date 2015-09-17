86a5e90c3b358d7c730e577b8e0e545690a22d17
Volker Dobler <dr.volker.dobler@gmail.com>
2015-09-17 14:47:18 +0200

    ht: generate the check documentation as HTML
    A deadly hack, works a bit.

--- ht/list-checks.bash
+++ ht/list-checks.bash
@@ -4,16 +4,18 @@ checks=$(oracle -pos ./check.go:#332 implements | \
     cut -f 2 | cut -d " " -f 6 | tr -d "*" | sort)
 
 
+echo "<html><head><title>Checks</title></head>"
+echo "<body><h1>Available Checks</h1>"
+
 for c in $checks; do
     [[ -n "$c" ]] || continue
-    figlet -W -w 100 "$c"
-    go doc "$c"
-
-    echo
+    echo "<h2>$c</h2>"
+    go doc "$c" \
+	| sed -e 's,^\(type.*{\)$,<pre>\1,' \
+	| sed -e 's,^\(type.*\)$,<pre>\1</pre>,' \
+	| sed -e "s,^},}</pre>,"
     echo
 done | grep -v -E "^func "
 
-echo "===================================================================================================="
 
-figlet -W -w 100 "Conditon"
-go doc Condition | grep -v -E "^func "
+echo "</body></html>"
