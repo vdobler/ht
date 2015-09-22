3716b548b961c82a2ebfdb632fd141f0f0a1ae51
Volker Dobler <dr.volker.dobler@gmail.com>
2015-09-22 09:16:27 +0200

    all: produce proper HTML documentation for checks

--- ht/list-checks.bash
+++ ht/list-checks.bash
@@ -3,19 +3,27 @@
 checks=$(oracle -pos ./check.go:#332 implements | \
     cut -f 2 | cut -d " " -f 6 | tr -d "*" | sort)
 
-
-echo "<html><head><title>Checks</title></head>"
+echo '<!DOCTYPE html>'
+echo '<html><head><title>Availbale Checks</title><meta charset="UTF-8"></head>'
 echo "<body><h1>Available Checks</h1>"
 
-for c in $checks; do
+echo "<p>Version: $(git describe)</p>"
+
+for c in $checks Condition; do
     [[ -n "$c" ]] || continue
+    if [[ "$c" = "Condition" ]];then
+	echo "<hr>"
+	echo "<p>Type Condition is not a Check but it is used so often"
+	echo "   in checks that it is worth describing here.</p>"
+    fi
     echo "<h2>$c</h2>"
+    echo "<pre>"
     go doc "$c" \
-	| sed -e 's,^\(type.*{\)$,<pre>\1,' \
-	| sed -e 's,^\(type.*\)$,<pre>\1</pre>,' \
-	| sed -e "s,^},}</pre>,"
-    echo
+	| sed -e "s/&/\\&amp;/g; s/</\\&lt;/g; s/>/\\&gt;/g"
+    echo "</pre>"
 done | grep -v -E "^func "
+
+
 
 
 echo "</body></html>"
