76393fbc6083df9061f5030833e6bcebc61a969a
Volker Dobler <dr.volker.dobler@gmail.com>
2015-11-19 11:34:11 +0100

    recorder: make reverse proxy more robust
    Set Host field in request to the target hostname as this used by many
    applications.
    Try to force all requests from the target server by disabeling caching.
    Keep both (request and response) bodies in an event to make code clearer.
    Do not generate content tests of no response body is present.

--- recorder/recorder.go
+++ recorder/recorder.go
@@ -1,8 +1,9 @@
 // Copyright 2015 Volker Dobler.  All rights reserved.
+// Copyright 2011 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// recorder is allows to capture request/response pairs via a
+// Package recorder allows to capture request/response pairs via a
 // reverse proxy and generate tests for these pairs.
 package recorder
 
@@ -20,6 +21,7 @@ import (
 	"os"
 	"path"
 	"regexp"
+	"sort"
 	"strings"
 	"time"
 
@@ -34,22 +36,29 @@ import (
 // Events is the global list of recorded events.
 var Events []Event
 
-// Event is a request/response pair
+// Event is a request/response pair.
 type Event struct {
-	Request   *http.Request              // The request
-	Response  *httptest.ResponseRecorder // The recorded response
-	Body      []byte                     // The captured body
-	Timestamp time.Time                  // Timestamp when caputred
-	Name      string                     // Used during dumping
+	Request      *http.Request              // The request.
+	Response     *httptest.ResponseRecorder // The recorded response.
+	RequestBody  string                     // The captured body.
+	ResponseBody string
+	Timestamp    time.Time // Timestamp when caputred.
+	Name         string    // Used during dumping.
 }
 
+// Options determining which events should be captured.
 type Options struct {
 	// Disarm is the time span after a captured request/response pair
 	// in which the capturing is disarmed.
 	Disarm time.Duration
 
+	// IgnoredContentType allows to skip capturing a request whose
+	// Content-Type header matches.
 	IgnoredContentType *regexp.Regexp
-	IgnoredPath        *regexp.Regexp
+
+	// IgnoredPath allows to skip capturing events based on the
+	// requested path,
+	IgnoredPath *regexp.Regexp
 }
 
 func (o Options) ignore(e Event) bool {
@@ -70,32 +79,74 @@ var (
 )
 
 // StartReverseProxy listens on the local port and forwards request to remote
-// while capturing the request/response pairs.
+// while capturing the request/response pairs selected by opts.
 func StartReverseProxy(port string, remote *url.URL, opts Options) error {
 	remoteHost = remote.Host
 	requests := make(chan Event, 10)
 	go process(requests, opts)
 
-	proxy := httputil.NewSingleHostReverseProxy(remote)
+	proxy := newSingleHostReverseProxy(remote)
 	http.HandleFunc("/", handler(proxy, requests))
 	log.Printf("Staring reverse proxying from localhost:%s to %s", port, remote.String())
 	return http.ListenAndServe(port, nil)
 }
 
-func handler(p *httputil.ReverseProxy, requests chan Event) func(http.ResponseWriter, *http.Request) {
+func newSingleHostReverseProxy(target *url.URL) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path = singleJoiningSlash(target.Path, req.URL.Path)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		// Additional to httputil.NewSingleHostReverseProxy:
+		// Fake host and disable caching.
+		req.Host = target.Host
+		// TODO: the next 3 are definitively usefull as response headers,
+		// but check if the can be used for requests too.
+		req.Header.Set("Cache-Control", "no-cache, no-store, must-revalidate")
+		req.Header.Set("Pragma", "no-cache")
+		req.Header.Set("Expires", "0")
+		req.Header.Del("If-Modified-Since")
+		req.Header.Del("If-None-Match")
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
+}
+
+// handler produces a http.HandlerFunc which routes the request via the
+// reverse proxy p, records the request and the response and sends these
+// to events.
+func handler(p *httputil.ReverseProxy, events chan Event) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		rr := httptest.NewRecorder()
-		body, err := ioutil.ReadAll(r.Body)
+		requestBody, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			panic(err.Error()) // Harsh but what else?
 		}
-		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(requestBody))
 		p.ServeHTTP(rr, r)
-		requests <- Event{
-			Request:   r,
-			Response:  rr,
-			Body:      body,
-			Timestamp: time.Now()}
+
+		events <- Event{
+			Request:      r,
+			RequestBody:  string(requestBody),
+			Response:     rr,
+			ResponseBody: rr.Body.String(),
+			Timestamp:    time.Now()}
 		for h, v := range rr.HeaderMap {
 			w.Header()[h] = v
 		}
@@ -104,10 +155,12 @@ func handler(p *httputil.ReverseProxy, requests chan Event) func(http.ResponseWr
 	}
 }
 
-func process(requests chan Event, opts Options) {
+// process drains events and decides whether too keep (i.e append to Events)
+// or ignore it.
+func process(events chan Event, opts Options) {
 	log.Println("Started processing")
 	last := time.Now()
-	for e := range requests {
+	for e := range events {
 		delta := e.Timestamp.Sub(last)
 		if delta < opts.Disarm {
 			continue
@@ -118,8 +171,22 @@ func process(requests chan Event, opts Options) {
 		last = e.Timestamp
 		e.Name = fmt.Sprintf("Event %d", len(Events)+1)
 		Events = append(Events, e)
-		log.Println("Recorded", e.Request.Method, e.Request.URL, " --> ", e.Response.Code)
+		log.Println("Recorded", e.Request.Method, e.Request.URL, " --> ",
+			e.Response.Code, e.Response.HeaderMap.Get("Content-Type"))
 	}
+}
+
+func printHeader(which string, header http.Header) {
+	hs := []string{}
+	for h := range header {
+		hs = append(hs, h)
+	}
+	sort.Strings(hs)
+	fmt.Println(which)
+	for _, h := range hs {
+		fmt.Printf("%20s :  %v\n", h, header[h])
+	}
+	fmt.Println()
 }
 
 // Test is a reduced version of ht.Test suitable for serialization to JSON5.
@@ -139,7 +206,7 @@ type Suite struct {
 	Variables   map[string]string
 }
 
-// DumpEvents writes events to directory, it extracts common headers.
+// DumpEvents writes events to directory, it extracts common request headers.
 func DumpEvents(events []Event, directory string, suitename string) error {
 	err := os.MkdirAll(directory, 0777)
 	if err != nil {
@@ -147,7 +214,7 @@ func DumpEvents(events []Event, directory string, suitename string) error {
 	}
 
 	// extract all common headers into mixin
-	commonHeaders := ExtractCommonHeaders(events)
+	commonHeaders := ExtractCommonRequestHeaders(events)
 	test := &Test{
 		Name: fmt.Sprintf("Common Header of %s", suitename),
 		Request: ht.Request{
@@ -184,7 +251,7 @@ func DumpEvents(events []Event, directory string, suitename string) error {
 		urlString = strings.Replace(urlString, "H.O.S.T.N.A.M.E", "{{HOSTNAME}}", 1)
 
 		// TODO: scan body for parameters and set ParamsAs
-		body := e.Body
+		body := e.RequestBody
 
 		checks := extractChecks(e)
 
@@ -291,21 +358,26 @@ func extractChecks(e Event) ht.CheckList {
 		list = append(list, red)
 	}
 
-	// Based on content type
-	switch {
-	case contentTypeParts[1] == "html", contentTypeParts[1] == "xhtml":
-		list = append(list, extractHTMLChecks(e)...)
-	case contentTypeParts[0] == "image":
-		list = append(list, extractImageChecks(e)...)
-	case contentTypeParts[1] == "pdf":
-		list = append(list, identityCheck(e))
+	// Based on content type but ignore responses without body (e.g. 301)
+	if len(e.ResponseBody) > 0 {
+		switch {
+		case contentTypeParts[1] == "html", contentTypeParts[1] == "xhtml":
+			list = append(list, extractHTMLChecks(e)...)
+		case contentTypeParts[0] == "image":
+			list = append(list, extractImageChecks(e)...)
+		case contentTypeParts[1] == "pdf":
+			list = append(list, identityCheck(e))
+		}
 	}
 
 	return list
 }
 
+// ----------------------------------------------------------------------------
+// Content based checks
+
 func identityCheck(e Event) ht.Check {
-	hash := sha1.Sum(e.Body)
+	hash := sha1.Sum([]byte(e.ResponseBody))
 	return ht.Identity{SHA1: fmt.Sprintf("%02x", hash)}
 }
 
@@ -327,8 +399,9 @@ func extractHTMLChecks(e Event) ht.CheckList {
 		},
 	})
 
-	doc, err := html.Parse(bytes.NewBuffer(e.Body))
+	doc, err := html.Parse(bytes.NewBufferString(e.ResponseBody))
 	if err != nil {
+		log.Println(err)
 		return list
 	}
 
@@ -337,6 +410,7 @@ func extractHTMLChecks(e Event) ht.CheckList {
 
 	// Title
 	if node := htmlTitleSel.MatchFirst(doc); node != nil {
+		log.Printf("Title matched")
 		title := ht.TextContent(node, false)
 		list = append(list, &ht.HTMLContains{
 			Selector: "head title",
@@ -347,12 +421,13 @@ func extractHTMLChecks(e Event) ht.CheckList {
 
 	// All h1
 	if nodes := htmlH1Sel.MatchAll(doc); len(nodes) != 0 {
+		log.Printf("H1 matched")
 		h1s := []string{}
 		for _, node := range nodes {
 			h1s = append(h1s, ht.TextContent(node, false))
 		}
 		list = append(list, &ht.HTMLContains{
-			Selector: "head title",
+			Selector: "body h1",
 			Text:     h1s,
 			Complete: true,
 			InOrder:  true,
@@ -365,7 +440,7 @@ func extractHTMLChecks(e Event) ht.CheckList {
 func extractImageChecks(e Event) ht.CheckList {
 	list := ht.CheckList{}
 
-	image, format, err := image.Decode(bytes.NewBuffer(e.Body))
+	image, format, err := image.Decode(bytes.NewBufferString(e.ResponseBody))
 	if err != nil {
 		return list
 	}
@@ -390,6 +465,9 @@ func extractImageChecks(e Event) ht.CheckList {
 
 	return list
 }
+
+// ----------------------------------------------------------------------------
+// Cookie handling
 
 func cookiePath(c *http.Cookie, u *url.URL) string {
 	if c.Path != "" {
@@ -439,23 +517,42 @@ func createSetCookieCheck(c *http.Cookie, now time.Time) *ht.SetCookie {
 	return sc
 }
 
-// ExtractCommonHeaders from events.
-func ExtractCommonHeaders(events []Event) http.Header {
+// ----------------------------------------------------------------------------
+// Extraction of common header fields
+
+func ExtractCommonResponseHeaders(events []Event) http.Header {
+	headers := make([]http.Header, len(events))
+	for i := range events {
+		headers[i] = events[i].Response.HeaderMap
+	}
+	return extractCommonHeaders(headers)
+}
+
+func ExtractCommonRequestHeaders(events []Event) http.Header {
+	headers := make([]http.Header, len(events))
+	for i := range events {
+		headers[i] = events[i].Request.Header
+	}
+	return extractCommonHeaders(headers)
+}
+
+// extractCommonHeaders collects and returns all common header entries in
+// headers an deletes the common one from headers.
+func extractCommonHeaders(headers []http.Header) http.Header {
 	common := http.Header{}
-	h0 := events[0].Request.Header
-	for h, v := range h0 {
+	for h, v := range headers[0] {
 		vs := fmt.Sprintf("%v", v)
 		identical := true
-		for j := 2; j < len(events); j++ {
-			if vs != fmt.Sprintf("%v", events[j].Request.Header[h]) {
+		for j := 2; j < len(headers); j++ {
+			if vs != fmt.Sprintf("%v", headers[j][h]) {
 				identical = false
 				break
 			}
 		}
 		if identical {
 			common[h] = v
-			for _, e := range events {
-				e.Request.Header.Del(h)
+			for i := range headers {
+				headers[i].Del(h)
 			}
 		}
 	}
