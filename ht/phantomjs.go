53ef9da1107df39c03f31c56829526473302d83d
Volker Dobler <dr.volker.dobler@gmail.com>
2016-08-02 08:59:09 +0200

    ht: fix comment

--- ht/phantomjs.go
+++ ht/phantomjs.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// latency.go contains checks against response time latency.
+// phantomjs.go contains checks through PhantomJS (phantomjs.org)
 
 package ht
 
