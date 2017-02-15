946b8ef61734a82df349b05f0da595652beb2ad5
Volker Dobler <dr.volker.dobler@gmail.com>
2017-02-15 14:42:09 +0100

    ht: fix package doc
    The package doc was horribly outdated.

--- ht/doc.go
+++ ht/doc.go
@@ -15,6 +15,7 @@
 // a way to allow easy deserialisation from a text format. This allows to load
 // and execute whole Suites at runtime.
 //
+//
 // Checks
 //
 // A typical check validates a certain property of the received response.
@@ -28,25 +29,40 @@
 // Forbidding the occurenc of "foobar" thus requires a negative Count.
 //
 // The following checks are provided
-//     * Body            checks text in the response body
-//     * ContentType     checks Content-Type header
-//     * DeleteCookie    checks for proper deletion of cookies
-//     * FinalURL        checks final URL after a redirect chain
-//     * Header          checks presence and values of received HTTP header
-//     * HTMLContains    checks text content of CSS-selected elements
-//     * HTMLTag         checks occurrence HTML elements chosen via CSS-selectors
-//     * Identity        checks the SHA1 hash of the HTTP body
-//     * Image           checks image format, size and content
-//     * JSON            checks structure and content of a JSON body
-//     * Links           checks accesability of hrefs and srcs in HTML
-//     * Logfile         checks data written to a logfile
-//     * Redirect        checks for redirections
-//     * ResponseTime    checks lower and higher bounds on the response time
-//     * SetCookie       checks properties of received cookies
-//     * StatusCode      checks the received HTTP status code
-//     * UTF8Encoded     checks that the HTTP body is UTF-8 encoded
-//     * W3CValidHTML    checks if body parses as valid HTML5
-//     * XML             checks elements of a XML body
+//     * AnyOne          logical OR of several tests
+//     * Body            text in the response body
+//     * ContentType     Content-Type header
+//     * CustomJS        performed by your own JavaScript code
+//     * DeleteCookie    for proper deletion of cookies
+//     * FinalURL        final URL after a redirect chain
+//     * Header          presence and values of received HTTP header
+//     * HTMLContains    text content of CSS-selected elements
+//     * HTMLTag         occurrence HTML elements chosen via CSS-selectors
+//     * Header          HTTP header fields
+//     * Identity        the SHA1 hash of the HTTP body
+//     * Image           image format, size and content
+//     * JSON            structure and content of a JSON body
+//     * JSONExpr        structure and content of a JSON body
+//     * Latency         latency distribution of a request
+//     * Links           accesability of hrefs and srcs in HTML
+//     * Logfile         data written to a logfile
+//     * NoServerError   no timeout and no 5xx status code
+//     * None            logical NAND
+//     * Redirect        redirection
+//     * RedirectChain   several redirections
+//     * RenderedHTML    HTML after rendering via PhantomJS
+//     * RenderingTime   time to render page via PhantomJS
+//     * Resilience      how wellbehaved does the server answer modified requests
+//     * ResponseTime    lower and higher bounds on the response time
+//     * Screenshot      render screen via PhantomJS and compare to reference
+//     * SetCookie       properties of received cookies
+//     * Sorted          sorted occurence of text on body
+//     * StatusCode      the received HTTP status code
+//     * UTF8Encoded     that the HTTP body is UTF-8 encoded
+//     * ValidHTML       not obviousely malformed HTML
+//     * W3CValidHTML    if body parses as valid HTML5
+//     * XML             elements of a XML body
+//
 //
 // Requests
 //
@@ -57,62 +73,16 @@
 // All the ugly stuff like parameter encoding, generation of multipart
 // bodies, etc. are hidden from the user.
 //
-// Parametrisations
-//
-// Hardcoding e.g. the hostname in a test has obvious drawbacks. To overcome
-// these the Request and Checks may be parametrised. This is done by a simple
-// variable expansion in which occurrences of variables are replaced by their
-// values. Variables may occur in all (exported) string fields of Checks and
-// all suitable string fields of Request in the form:
-//     {{VARNAME}}
-// The variable substitution is performed during compilation of a Test which
-// includes compilation of the embedded Checks.
-//
-// The current time with an optional offset can be substituted by a special
-// construct:
-//     {{NOW}}                       -->  Wed, 01 Oct 2014 12:22:36 CEST
-//     {{NOW + 15s}}                 -->  Wed, 01 Oct 2014 12:22:51 CEST
-//     {{NOW + 25m | "15:04"}}       -->  12:47
-//     {{NOW + 3d | "2006-Jan-02"}}  -->  2014-Oct-04
-// Formating the time is done with the usual reference time of package time
-// and defaults to RFC1123. Offset can be negative, the known units are "s" for
-// seconds, "m" for minutes, "h" for hours and "d" for days.
-//
-// Some random values can be include by the following syntax:
-//     {{RANDOM NUMBER 99}}          -->  22
-//     {{RANDOM NUMBER 32-99}}       -->  45
-//     {{RANDOM NUMBER 99 %04x}}     -->  002d
-//     {{RANDOM TEXT 8}}             -->  que la victoire Accoure à tes mâles
-//     {{RANDOM TEXT 2-5}}           -->  Accoure à tes
-//     {{RANDOM TEXT de 5}}          -->  Denn die fromme Seele
-//     {{RANDOM EMAIL}}              -->  Leon.Schneider@gmail.com
-//     {{RANDOM EMAIL web.de}}       -->  Meier.Anna@web.de
 //
 // Tests
 //
 // A Test is basically just a Request combined with a list of Checks.
 // Running a Test is executing the request and validating the response
-// according to the Checks. Before a test can be run the variable substitution
-// in the Request and the Checks have to happen, a real HTTP request
-// has to be crafted and checks have to be set up. This is done by compiling
-// the test, a step which may fail: a) if the Request is malformed (e.g. uses
-// a malformed URL) or b) if the checks are malformed (e.g. uses a malformed
-// regexp). Such Tests/Checks are labeled Bogus.
+// according to the Checks.
 //
 // There are three ways in which a Tests may fail:
 //   1. The test setup is malformed, such tests are called Bogus.
-//   2. The request itself fails. This is called an Error
-//   3. Any of the checks fail. This is called a Failure
-//
-//
-// Suites of tests
-//
-// Normaly tests are not run individually but grouped into suites.
-// Such suite may share a common cookie jar (and a common logger)
-// and may contain setup and teardown actions. Any action on a Suite
-// normally requires its setup tests to pass, afterwards the main tests
-// are executed and finally the teardown tests are run (but no errors or
-// failures are reported for teardown tests).
-//
+//   2. The request itself fails, e.g. due to a timeout. This is called an Error.
+//   3. Any of the checks fail. This is called a Failure.
 //
 package ht
