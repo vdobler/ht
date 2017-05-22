4a58d910dc41e5a012c1967b8f9cdee3423f77f5
Volker Dobler <dr.volker.dobler@gmail.com>
2017-05-22 09:28:20 +0200

    cmd/mock: add real test for mocking

--- cmd/ht/testdata/test-mocks.bash
+++ cmd/ht/testdata/test-mocks.bash
@@ -3,22 +3,49 @@
 # test-mocks.bash test the mock subcommand of cmd/ht
 #
 
+set -euo pipefail
+
 # Start of mock service
-
-../ht mock -D POST="good buy" -cert ../../../mock/testdata/dummy.cert -key ../../../mock/testdata/dummy.key ../../../mock/testdata/body.mock &
-
+../ht mock -D POST="Ciao" -cert ../../../mock/testdata/dummy.cert -key ../../../mock/testdata/dummy.key ../../../mock/testdata/body.mock &
 pid=$!
+sleep 2
+kill -0 $pid || (echo "Problems running mock server."; echo "FAIL"; exit 1;)
 
-sleep 1
+function finish {
+    rm -f alf.de alf.en headmaster.de headmaster.en indiana.de indiana.en combined expected
+    kill -TERM $pid
+}
+trap finish EXIT
 
-for lastname in Alf Jones Dumbledore; do
-    curl -k -d last=$lastname -d first=Mike https://localhost:8880/de/greet
-
-    echo
-done
-
-
-
+curl -# -k -d first="Gordon" -d last="Shumway" https://localhost:8880/de/greet > alf.de
+curl -# -k -d first="Gordon" -d last="Shumway" https://localhost:8880/en/greet > alf.en
+curl -# -k -d first="Henry Walton" -d last="Jones" https://localhost:8880/de/greet > indiana.de
+curl -# -k -d first="Henry Walton" -d last="Jones" https://localhost:8880/en/greet > indiana.en
+curl -# -k -d first="Albus" -d last="Dumbledore" https://localhost:8880/de/greet > headmaster.de
+curl -# -k -d first="Albus" -d last="Dumbledore" https://localhost:8880/en/greet > headmaster.en
 
 
-kill -TERM $pid
+cat alf.de alf.en indiana.de indiana.en headmaster.de headmaster.en > combined
+
+cat > expected <<EOF
+Guten Tag Gordon Shumway
+Ciao
+Welcome Gordon Shumway
+Ciao
+Guten Tag Dr. Henry Walton 'Indiana' Jones
+Ciao
+Welcome Henry Walton Jones
+Ciao
+Guten Tag Prof. Albus Dumbledore
+Ciao
+Welcome Albus Dumbledore
+Ciao
+EOF
+
+if ! diff -u combined expected ; then
+    echo "Wrong output"
+    echo "FAIL"
+    exit 1
+fi
+echo "PASS"
+
