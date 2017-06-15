1f6f939d1d957f0113a4ff8deebbd4e11c46e3d7
Volker Dobler <dr.volker.dobler@gmail.com>
2017-06-15 16:21:41 +0200

    cmd/ht: restructure source code
    No code change, just code moves (and one new extracted function).
    The code was badly organized after organic growth and hurdled refactoring.

--- cmd/ht/exec.go
+++ cmd/ht/exec.go
@@ -83,6 +83,247 @@ func runExecute(cmd *Command, suites []*suite.RawSuite) {
 	errors = errors.Append(err)
 	err = reportOverall(outcome)
 	errors = errors.Append(err)
+
+	terminate(outcome, errors)
+}
+
+// PrepareHT and print diagnostics.
+func prepareHT() {
+	// Set several parameters of package ht.
+	if randomSeed == 0 {
+		randomSeed = time.Now().UnixNano()
+	}
+	scope.ResetCounter <- counterSeed
+	scope.Random = rand.New(rand.NewSource(randomSeed))
+	ht.PhantomJSExecutable = phantomjs
+	if !silent {
+		fmt.Printf("Seeding random number generator with %d.\n", randomSeed)
+		fmt.Printf("Resetting global counter to %d.\n", counterSeed)
+		fmt.Printf("Using %q as PhantomJS executable.\n", phantomjs)
+	}
+	if skipTLSVerify {
+		if !silent {
+			fmt.Println("Skipping verification of TLS certificates presented by any server.")
+		}
+		ht.Transport.TLSClientConfig.InsecureSkipVerify = true
+	}
+
+	if !silent {
+		// Log variables and values sorted by variable name.
+		varnames := make([]string, 0, len(variablesFlag))
+		for v := range variablesFlag {
+			varnames = append(varnames, v)
+		}
+		sort.Strings(varnames)
+		for _, v := range varnames {
+			fmt.Printf("Variable %s = %q\n", v, variablesFlag[v])
+		}
+	}
+}
+
+// Panics if selected output dir cannot be created.
+func prepareOutputDir() {
+	if outputDir == "/dev/null" {
+		mute = true
+	} else if outputDir == "" {
+		outputDir = time.Now().Format("2006-01-02_15h04m05s")
+	}
+
+	if !mute {
+		err := os.MkdirAll(outputDir, 0766)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+}
+
+// execute suites one by one saving each suite to disk once finished.
+// Returns the accumulated overall result.
+func executeSuites(suites []*suite.RawSuite, variables map[string]string, jar *cookiejar.Jar) (*accumulator, error) {
+	bufferedStdout := bufio.NewWriterSize(os.Stdout, 256)
+	defer bufferedStdout.Flush()
+	logger := log.New(bufferedStdout, "", 0)
+	errors := ht.ErrorList{}
+	var err error
+
+	if !mute {
+		if outputDir == "" {
+			outputDir = time.Now().Format("2006-01-02_15h04m05s")
+		}
+		err = os.MkdirAll(outputDir, 0766)
+		errors = errors.Append(err)
+	}
+
+	accum := newAccumulator()
+	for i, s := range suites {
+		if !ssilent {
+			logger.Println("Starting Suite", i+1, s.Name, s.File.Name)
+		}
+		outcome := s.Execute(variables, jar, logger)
+		bufferedStdout.Flush()
+
+		accum.update(outcome)
+
+		if carryVars {
+			variables = outcome.FinalVariables // carry over variables ???
+		}
+		if !silent {
+			err = outcome.PrintReport(os.Stdout)
+		} else if !ssilent {
+			err = outcome.PrintShortReport(os.Stdout)
+			fmt.Println()
+		}
+		errors = errors.Append(err)
+
+		err = saveSingle(outputDir, outcome)
+		errors = errors.Append(err)
+		if len(suites) > 1 {
+			err := saveOverallReport(outputDir, accum)
+			errors = errors.Append(err)
+		}
+
+	}
+	return accum, errors.AsError()
+}
+
+// ----------------------------------------------------------------------------
+// Reporting functions
+
+// saveSingle takes care of dumping the suite s into a subfolder of
+// outputdir. It will produce:
+//     _Report_.html  with accomaning files for the response bodies
+//     junit-report.xml
+//     result.txt
+//     variables.json
+//     cookies.json
+func saveSingle(outputDir string, s *suite.Suite) error {
+	if mute || outputDir == "/dev/null" {
+		return nil
+	}
+
+	dirname := path.Join(outputDir, sanitize.Filename(s.Name))
+	fmt.Printf("Saving result of suite %q to folder %q.\n", s.Name, dirname)
+	err := os.MkdirAll(dirname, 0766)
+	if err != nil {
+		return err
+	}
+
+	errors := ht.ErrorList{}
+	err = suite.HTMLReport(dirname, s)
+	errors = errors.Append(err)
+
+	file, err := os.Create(path.Join(dirname, "result.txt"))
+	errors = errors.Append(err)
+	if err == nil {
+		err = s.PrintReport(file)
+		errors = errors.Append(err)
+		errors = errors.Append(file.Close())
+	}
+
+	cwd, err := os.Getwd()
+	errors = errors.Append(err)
+	reportURL := "file://" + path.Join(cwd, dirname, "_Report_.html")
+	fmt.Printf("See %s\n", reportURL)
+
+	junit, err := s.JUnit4XML()
+	errors = errors.Append(err)
+	if err == nil {
+		err = ioutil.WriteFile(path.Join(dirname, "junit-report.xml"),
+			[]byte(junit), 0666)
+		errors = errors.Append(err)
+	}
+
+	// TODO: handle errors
+	err = saveVariables(s.FinalVariables, path.Join(dirname, "variables.json"))
+	errors = errors.Append(err)
+	err = saveCookiesFromJar(s.Jar, path.Join(dirname, "cookies.json"))
+	errors = errors.Append(err)
+
+	return errors.AsError()
+}
+
+// reportOverall summary data to stdout and save final variables and cookies.
+func reportOverall(a *accumulator) error {
+	var errors ht.ErrorList
+	var err error
+	// Save consolidated variables if required.
+	if vardump != "" && !mute {
+		err = saveVariables(a.vars, vardump)
+		errors = errors.Append(err)
+	}
+
+	// Save consolidated cookies if required.
+	if cookiedump != "" && !mute {
+		err = saveCookies(a.cookies, cookiedump)
+		errors = errors.Append(err)
+	}
+
+	if !ssilent {
+		fmt.Println()
+		fmt.Printf("Total %d,  Passed %d,  Skipped %d,  Errored %d,  Failed %d,  Bogus %d\n",
+			a.total, a.pass, a.skip, a.err, a.fail, a.bogus)
+		fmt.Println(strings.ToUpper(a.status.String()))
+	}
+
+	return errors.AsError()
+}
+
+// TODO: handle errors, preparse template, cleanup mess, learn to program
+func saveOverallReport(dirname string, accum *accumulator) error {
+	if mute {
+		return nil
+	}
+
+	tmplSrc := `<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <style>
+   .PASS { color: green; }
+   .FAIL { color: red; }
+   .ERROR { color: magenta; }
+   .NOTRUN { color: grey; }
+  </style>
+  <title>Overall Results</title>
+</head>
+<body>
+  <h1>Overall Result</h1>
+  <ul>
+    {{range .}}
+    <li>
+      <h2>
+        <a href="./{{.Path}}/_Report_.html" >
+          <span class="{{.Status}}">{{.Status}}</span> {{.Name}}
+        </a>
+      </h2>
+    </li>
+    {{end}}
+  </ul>
+</body>
+</html>
+`
+	templ := template.Must(template.New("report").Parse(tmplSrc))
+	buf := &bytes.Buffer{}
+	err := templ.Execute(buf, accum.suites)
+	if err != nil {
+		return err
+	}
+
+	file := dirname + "/_Report_.html"
+	if err := ioutil.WriteFile(file, buf.Bytes(), 0666); err != nil {
+		log.Panicf("Failed to write file: %q with error %s", file, err)
+	}
+	cwd, err := os.Getwd()
+	if err == nil {
+		reportURL := "file://" + path.Join(cwd, file)
+		fmt.Printf("Overall: %s\n", reportURL)
+	}
+	return err
+}
+
+// terminate cmd/ht with proper exit status depending in present errors and
+// the outcome of the executed suites.
+func terminate(outcome *accumulator, errors ht.ErrorList) {
 	if errors.AsError() != nil {
 		fmt.Fprintln(os.Stderr, "Error encountered during execution:")
 		for _, msg := range errors.AsStrings() {
@@ -104,22 +345,73 @@ func runExecute(cmd *Command, suites []*suite.RawSuite) {
 
 }
 
-func prepareOutputDir() {
-	if outputDir == "/dev/null" {
-		mute = true
-	} else if outputDir == "" {
-		outputDir = time.Now().Format("2006-01-02_15h04m05s")
-	}
+// ----------------------------------------------------------------------------
+// Helpers for saving and loading variables and cookies.
 
-	if !mute {
-		err := os.MkdirAll(outputDir, 0766)
-		if err != nil {
-			log.Panic(err)
-		}
+func saveVariables(vars map[string]string, filename string) error {
+	if mute {
+		return nil
 	}
+	b, err := json.MarshalIndent(vars, "    ", "")
+	if err != nil {
+		return nil
+	}
+	return ioutil.WriteFile(filename, b, 0666)
 }
 
-// accumulator of several suites.
+func saveCookiesFromJar(jar *cookiejar.Jar, filename string) error {
+	if jar == nil {
+		return nil
+	}
+
+	cookies := make(map[string]cookiejar.Entry)
+	for _, tld := range jar.ETLDsPlus1(nil) {
+		for _, cookie := range jar.Entries(tld, nil) {
+			id := cookie.ID()
+			cookies[id] = cookie
+		}
+	}
+	return saveCookies(cookies, filename)
+}
+
+func saveCookies(cookies map[string]cookiejar.Entry, filename string) error {
+	if mute {
+		return nil
+	}
+	b, err := json.MarshalIndent(cookies, "    ", "")
+	if err != nil {
+		return nil
+	}
+	return ioutil.WriteFile(filename, b, 0666)
+}
+
+func loadCookies() *cookiejar.Jar {
+	if cookie == "" {
+		return nil
+	}
+	buf, err := ioutil.ReadFile(cookie)
+	if err != nil {
+		log.Panicf("Cannot read cookie file: %s", err)
+	}
+
+	cookies := make(map[string]cookiejar.Entry)
+	err = json.Unmarshal(buf, &cookies)
+	if err != nil {
+		log.Panicf("Cannot decode cookie file: %s", err)
+	}
+	cs := make([]cookiejar.Entry, 0, len(cookies))
+	for _, c := range cookies {
+		cs = append(cs, c)
+	}
+
+	jar, _ := cookiejar.New(nil)
+	jar.LoadEntries(cs)
+	return jar
+}
+
+// ----------------------------------------------------------------------------
+// Accumulator of several suites to collect statistical summaries.
+
 type accumulator struct {
 	status  ht.Status
 	vars    map[string]string
@@ -186,278 +478,4 @@ func (a *accumulator) update(s *suite.Suite) {
 		}
 		a.total++
 	}
-}
-
-func reportOverall(a *accumulator) error {
-	var errors ht.ErrorList
-	var err error
-	// Save consolidated variables if required.
-	if vardump != "" && !mute {
-		err = saveVariables(a.vars, vardump)
-		errors = errors.Append(err)
-	}
-
-	// Save consolidated cookies if required.
-	if cookiedump != "" && !mute {
-		err = saveCookies(a.cookies, cookiedump)
-		errors = errors.Append(err)
-	}
-
-	if !ssilent {
-		fmt.Println()
-		fmt.Printf("Total %d,  Passed %d,  Skipped %d,  Errored %d,  Failed %d,  Bogus %d\n",
-			a.total, a.pass, a.skip, a.err, a.fail, a.bogus)
-		fmt.Println(strings.ToUpper(a.status.String()))
-	}
-
-	return errors.AsError()
-}
-
-func saveVariables(vars map[string]string, filename string) error {
-	if mute {
-		return nil
-	}
-	b, err := json.MarshalIndent(vars, "    ", "")
-	if err != nil {
-		return nil
-	}
-	return ioutil.WriteFile(filename, b, 0666)
-}
-
-func saveCookiesFromJar(jar *cookiejar.Jar, filename string) error {
-	if jar == nil {
-		return nil
-	}
-
-	cookies := make(map[string]cookiejar.Entry)
-	for _, tld := range jar.ETLDsPlus1(nil) {
-		for _, cookie := range jar.Entries(tld, nil) {
-			id := cookie.ID()
-			cookies[id] = cookie
-		}
-	}
-	return saveCookies(cookies, filename)
-}
-
-func saveCookies(cookies map[string]cookiejar.Entry, filename string) error {
-	if mute {
-		return nil
-	}
-	b, err := json.MarshalIndent(cookies, "    ", "")
-	if err != nil {
-		return nil
-	}
-	return ioutil.WriteFile(filename, b, 0666)
-}
-
-// TODO: handle errors, preparse template, cleanup mess, learn to program
-func saveOverallReport(dirname string, accum *accumulator) error {
-	if mute {
-		return nil
-	}
-
-	tmplSrc := `<!DOCTYPE html>
-<html>
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-  <style>
-   .PASS { color: green; }
-   .FAIL { color: red; }
-   .ERROR { color: magenta; }
-   .NOTRUN { color: grey; }
-  </style>
-  <title>Overall Results</title>
-</head>
-<body>
-  <h1>Overall Result</h1>
-  <ul>
-    {{range .}}
-    <li>
-      <h2>
-        <a href="./{{.Path}}/_Report_.html" >
-          <span class="{{.Status}}">{{.Status}}</span> {{.Name}}
-        </a>
-      </h2>
-    </li>
-    {{end}}
-  </ul>
-</body>
-</html>
-`
-	templ := template.Must(template.New("report").Parse(tmplSrc))
-	buf := &bytes.Buffer{}
-	err := templ.Execute(buf, accum.suites)
-	if err != nil {
-		return err
-	}
-
-	file := dirname + "/_Report_.html"
-	if err := ioutil.WriteFile(file, buf.Bytes(), 0666); err != nil {
-		log.Panicf("Failed to write file: %q with error %s", file, err)
-	}
-	cwd, err := os.Getwd()
-	if err == nil {
-		reportURL := "file://" + path.Join(cwd, file)
-		fmt.Printf("Overall: %s\n", reportURL)
-	}
-	return err
-}
-
-func prepareHT() {
-	// Set several parameters of package ht.
-	if randomSeed == 0 {
-		randomSeed = time.Now().UnixNano()
-	}
-	scope.ResetCounter <- counterSeed
-	scope.Random = rand.New(rand.NewSource(randomSeed))
-	ht.PhantomJSExecutable = phantomjs
-	if !silent {
-		fmt.Printf("Seeding random number generator with %d.\n", randomSeed)
-		fmt.Printf("Resetting global counter to %d.\n", counterSeed)
-		fmt.Printf("Using %q as PhantomJS executable.\n", phantomjs)
-	}
-	if skipTLSVerify {
-		if !silent {
-			fmt.Println("Skipping verification of TLS certificates presented by any server.")
-		}
-		ht.Transport.TLSClientConfig.InsecureSkipVerify = true
-	}
-
-	if !silent {
-		// Log variables and values sorted by variable name.
-		varnames := make([]string, 0, len(variablesFlag))
-		for v := range variablesFlag {
-			varnames = append(varnames, v)
-		}
-		sort.Strings(varnames)
-		for _, v := range varnames {
-			fmt.Printf("Variable %s = %q\n", v, variablesFlag[v])
-		}
-	}
-}
-
-func loadCookies() *cookiejar.Jar {
-	if cookie == "" {
-		return nil
-	}
-	buf, err := ioutil.ReadFile(cookie)
-	if err != nil {
-		log.Panicf("Cannot read cookie file: %s", err)
-	}
-
-	cookies := make(map[string]cookiejar.Entry)
-	err = json.Unmarshal(buf, &cookies)
-	if err != nil {
-		log.Panicf("Cannot decode cookie file: %s", err)
-	}
-	cs := make([]cookiejar.Entry, 0, len(cookies))
-	for _, c := range cookies {
-		cs = append(cs, c)
-	}
-
-	jar, _ := cookiejar.New(nil)
-	jar.LoadEntries(cs)
-	return jar
-}
-
-// execute suites one by one saving each suite to disk once finished.
-// Returns the accumulated overall result.
-func executeSuites(suites []*suite.RawSuite, variables map[string]string, jar *cookiejar.Jar) (*accumulator, error) {
-	bufferedStdout := bufio.NewWriterSize(os.Stdout, 256)
-	defer bufferedStdout.Flush()
-	logger := log.New(bufferedStdout, "", 0)
-	errors := ht.ErrorList{}
-	var err error
-
-	if !mute {
-		if outputDir == "" {
-			outputDir = time.Now().Format("2006-01-02_15h04m05s")
-		}
-		err = os.MkdirAll(outputDir, 0766)
-		errors = errors.Append(err)
-	}
-
-	accum := newAccumulator()
-	for i, s := range suites {
-		if !ssilent {
-			logger.Println("Starting Suite", i+1, s.Name, s.File.Name)
-		}
-		outcome := s.Execute(variables, jar, logger)
-		bufferedStdout.Flush()
-
-		accum.update(outcome)
-
-		if carryVars {
-			variables = outcome.FinalVariables // carry over variables ???
-		}
-		if !silent {
-			err = outcome.PrintReport(os.Stdout)
-		} else if !ssilent {
-			err = outcome.PrintShortReport(os.Stdout)
-			fmt.Println()
-		}
-		errors = errors.Append(err)
-
-		err = saveSingle(outputDir, outcome)
-		errors = errors.Append(err)
-		if len(suites) > 1 {
-			err := saveOverallReport(outputDir, accum)
-			errors = errors.Append(err)
-		}
-
-	}
-	return accum, errors.AsError()
-}
-
-// saveSingle takes care of dumping the suite s into a subfolder of
-// outputdir. It will produce:
-//   _Report_.html  with accomaning files for the response bodies
-//   junit-report.xml
-//   result.txt
-//   variables.json
-//   cookies.json
-func saveSingle(outputDir string, s *suite.Suite) error {
-	if mute || outputDir == "/dev/null" {
-		return nil
-	}
-
-	dirname := path.Join(outputDir, sanitize.Filename(s.Name))
-	fmt.Printf("Saving result of suite %q to folder %q.\n", s.Name, dirname)
-	err := os.MkdirAll(dirname, 0766)
-	if err != nil {
-		return err
-	}
-
-	errors := ht.ErrorList{}
-	err = suite.HTMLReport(dirname, s)
-	errors = errors.Append(err)
-
-	file, err := os.Create(path.Join(dirname, "result.txt"))
-	errors = errors.Append(err)
-	if err == nil {
-		err = s.PrintReport(file)
-		errors = errors.Append(err)
-		errors = errors.Append(file.Close())
-	}
-
-	cwd, err := os.Getwd()
-	errors = errors.Append(err)
-	reportURL := "file://" + path.Join(cwd, dirname, "_Report_.html")
-	fmt.Printf("See %s\n", reportURL)
-
-	junit, err := s.JUnit4XML()
-	errors = errors.Append(err)
-	if err == nil {
-		err = ioutil.WriteFile(path.Join(dirname, "junit-report.xml"),
-			[]byte(junit), 0666)
-		errors = errors.Append(err)
-	}
-
-	// TODO: handle errors
-	err = saveVariables(s.FinalVariables, path.Join(dirname, "variables.json"))
-	errors = errors.Append(err)
-	err = saveCookiesFromJar(s.Jar, path.Join(dirname, "cookies.json"))
-	errors = errors.Append(err)
-
-	return errors.AsError()
 }
