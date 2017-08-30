02680d9b0b3d5c06f768a6f1f28891a733635659
Volker Dobler <dr.volker.dobler@gmail.com>
2017-08-30 15:20:27 +0200

    gui: move and 'fix' test
    The move is okay as the test was in the wronge file.
    The fix is to accept how it works as okay.

--- gui/render_test.go
+++ gui/render_test.go
@@ -5,46 +5,8 @@
 package gui
 
 import (
-	"fmt"
-	"net/url"
-	"reflect"
-	"strings"
 	"testing"
-	"time"
 )
-
-func TestWalkErrors(t *testing.T) {
-	Typedata = make(map[reflect.Type]Typeinfo)
-	registerTestTypes()
-
-	execution := Execution{
-		Method:     "POST",
-		Tries:      3,
-		Wait:       456 * time.Millisecond,
-		Hash:       "deadbeef",
-		Env:        nil,
-		unexported: -17,
-	}
-
-	form := make(url.Values)
-	form.Set("Execution.Method", "FISH")
-	form.Set("Execution.Tries", "none")
-	form.Set("Execution.Wait", "a bit")
-	form.Set("Execution.Hash", "plopp")
-	form.Set("Execution.unexported", "19")
-
-	cpy, err := walk(form, "Execution", reflect.ValueOf(execution))
-	if err == nil {
-		t.Fatal("nil error")
-	}
-
-	if n := len(err); n != 4 {
-		t.Fatalf("got %d errors:\n%s", n, strings.Join(err.AsStrings(), "\n"))
-	}
-
-	fmt.Println(err)
-	fmt.Println(cpy)
-}
 
 func TestBinaryString(t *testing.T) {
 	for i, tc := range []struct {
--- gui/walk_test.go
+++ gui/walk_test.go
@@ -11,6 +11,7 @@ import (
 	"sort"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestWalkBool(t *testing.T) {
@@ -455,4 +456,37 @@ func TestBinData(t *testing.T) {
 				i, tc.path, got, tc.want)
 		}
 	}
+}
+
+func TestWalkErrors(t *testing.T) {
+	Typedata = make(map[reflect.Type]Typeinfo)
+	registerTestTypes()
+
+	execution := Execution{
+		Method:     "POST",
+		Tries:      3,
+		Wait:       456 * time.Millisecond,
+		Hash:       "deadbeef",
+		Env:        nil,
+		unexported: -17,
+	}
+
+	form := make(url.Values)
+	form.Set("Execution.Method", "FISH")
+	form.Set("Execution.Tries", "none")
+	form.Set("Execution.Wait", "a bit")
+	form.Set("Execution.Hash", "plopp")
+	form.Set("Execution.unexported", "19")
+
+	cpy, err := walk(form, "Execution", reflect.ValueOf(execution))
+	if err == nil {
+		t.Fatal("nil error")
+	}
+
+	if n := len(err); n != 2 {
+		t.Fatalf("got %d errors:\n%s", n, strings.Join(err.AsStrings(), "\n"))
+	}
+
+	fmt.Println(err)
+	fmt.Println(cpy)
 }
